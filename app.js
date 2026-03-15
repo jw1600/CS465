77f76afb22f93940aa325138bd512b01f75d6794
jw1600 <161659780+jw1600@users.noreply.github.com>
2026-03-15 09:32:18 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('hbs');
+
+const indexRouter = require('./app_server/routes/index');
+const travelRouter = require('./app_server/routes/travel');
 
 const app = express();
-const port = 3000;
 
+// Register partials for Handlebars
+hbs.registerPartials(path.join(__dirname, 'app_server/views/partials'));
+
+app.set('views', path.join(__dirname, 'app_server', 'views'));
+app.set('view engine', 'hbs');
+
+app.use('/', indexRouter);
+app.use('/travel', travelRouter);
 app.use(express.static(path.join(__dirname, 'public')));
 
+const port = 3000;
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+    console.log('Server running on port ' + port);
 });
